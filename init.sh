22789661d0a3d8b55890deb6942557a1d271bacb
KimkimRakim <kim.the.fisherman@gmial.com>
2018-06-10 08:25:01 +0000

    add init.sh

---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#######################################
+# Description
+#######################################
+# This is for initial setup.
+# You should run this script only once at first.
+
+cd `dirname $0`
+#######################################
+# synbolic link
+#######################################
+#ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ${PWD}/.vimrc ~/.vimrc
+
+#######################################
+# Install vim-plug
+#######################################
+if [ -e ~/.vim/autoload/plug.vim ]; then
+  echo "Vim-plug is already installed"
+else
+  # Install vim-plug from git 
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  # Install plugin listed in .vimrc
+  vim -c :PlugInstall -c q! -c q!
+fi
+
